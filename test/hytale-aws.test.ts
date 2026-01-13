fbb224e897bc0f017eb600d0c764dfa541aa397f
David Tanner Jr <tannerdjr@gmail.com>
2026-01-13 14:36:33 -0500

    Initial commit

---
+++ test/hytale-aws.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HytaleAws from '../lib/hytale-aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hytale-aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HytaleAws.HytaleAwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
